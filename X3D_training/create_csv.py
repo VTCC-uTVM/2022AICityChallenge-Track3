35445001501ce73f03d7851a78152adf32de6aae
VTCC-uTVM <103914870+VTCC-uTVM@users.noreply.github.com>
2022-04-26 02:01:30 +0700

    Update create_csv.py

--- X3D_training/create_csv.py
+++ X3D_training/create_csv.py
@@ -7,9 +7,9 @@ k_map = {"dashboard": "Dashboard", "rearview": "Rear", "right": "Right"}
 for view in ["dashboard", "rearview", "right"]:
     for user_id in ["49381", "38058", "35133", "24491", "24026"]:
         f = open("total_data.csv", "r")
-        f_train = open("train_{}_{}.csv".format(view, user_id), "w")
-        f_val = open("val_{}_{}.csv".format(view, user_id), "w")
-        f_test = open("test_{}_{}.csv".format(view, user_id), "w")
+        f_train = open("data/train_{}_{}.csv".format(view, user_id), "w")
+        f_val = open("data/val_{}_{}.csv".format(view, user_id), "w")
+        f_test = open("data/test_{}_{}.csv".format(view, user_id), "w")
         for line in f.readlines():
             if k_map[view] in line:    
                 if user_id in line:
